7dfc82cfc6cc3ab4ea476e23876a7f64166f3d87
code4all2 <code4allaulaonline@gmail.com>
2023-07-17 12:12:18 -0300

    Adicionar arquivos de projeto.

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    private float speed = 5.0f;
+    private float guinada;
+
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
